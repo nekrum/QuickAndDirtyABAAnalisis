0147f70a21a7ea463f477432d5e9b2da7ac24a59
miusuario <miusuario@micorreo.com>
2019-10-02 14:27:57 -0500

    Incluye ayuda del paquete

---
+++ R/QuickAndDirtyABAAnalisis.R
@@ -0,0 +1,18 @@
+#' QuickAndDirtyABAAnalisis: Análisi Rápido y Facil de los datasets ABA
+#'
+#'Este paquete intenta facilitar la exploración de las librerías de expresión genética de adultos y humanos en desarrollo
+#'relacionados con el Allen Brain Atlas.
+#'No pretende suplantar el uso del paquete ABAEnrichment, solo simplificarlo para los alumnos de la clase de Diseño
+#'experimental en la licenciatura de Neurobiología.
+#'
+#' @section Funciones del paquete:
+#'
+#' GetGenAreas: Obtiene las áreas relacionadas con la expresión de un gen
+#'
+#' GetAreasGenes: Obtiene los genes relaciondados con una área
+#'
+#' GetDatasetAreasSimplified: Obtiene todas las areas en un dataset
+#'
+#' @docType package
+#' @name QuickAndDirtyABAAnalisis
+NULL
